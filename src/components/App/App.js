183912ee4c9b3f128c8d6e117b72517ae5e89acb
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-01-23 19:00:23 +0100

    Add first className

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './App.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <main class="app-content">
+      <main className={styles.component}>
         <h1>My first React app</h1>
         <h2>Hello World!!</h2>
       </main>
