ccae9b90a325bb38dc7a19c32069f22b52b43487
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-01-23 21:54:17 +0100

    Add columns

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List';
+import Columns from '../Column/Column';
 
 
 class App extends React.Component {
@@ -9,7 +10,12 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello World!!</h2>
-        <List/>
+        <List title={['Thing to do !', <sup key='1'> soon!!!</sup>]}
+              image={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+              columns={[]}>
+              
+        <p>I'm planning on doing all these things sooner, rather than later!</p>
+        </List>
       </main>
     )
   }
