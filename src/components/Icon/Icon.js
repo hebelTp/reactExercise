16897de4a4d9333a3f3bd86d7bd38ed00b168ae6
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2020-02-02 15:14:33 +0100

    Add routing between Home&Info

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
 const Icon = props => (<i className={`fas fa-${props.name}`}></i>);
+
+Icon.propTypes = {
+  name: PropsTypes.string,
+};
 
 export default Icon;
